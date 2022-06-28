b39b51298f2cf324e7e1b9edcd50e88bdf1ca41f
reinni23 <reinni@naver.com>
2022-06-28 14:10:46 +0900

    app.js수정

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var path = require('path');
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var path = require("path");
+const http = this.require("http");
 
-var routers = require('./router/route')
+setInterval(function () {
+  http.get("https://seomuntest23.herokuapp.com/");
+}, 600000);
+
+var routers = require("./router/route");
 
 var app = express();
 
-app.set('views',path.join(__dirname,'views')); //views라는 폴더안에 ejs들을 불러올거다. + views안에 ejs파일들을 만들어주면됨.
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views")); //views라는 폴더안에 ejs들을 불러올거다. + views안에 ejs파일들을 만들어주면됨.
+app.set("view engine", "ejs");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(logger('dev'));
-app.use(express.static(path.join(__dirname,'public'))); //public이라는 폴더에서 css와 img를 다 받아올것이라는 의미 + public폴더 안에 css폴더와 img폴더를 넣어주면됨.
+app.use(logger("dev"));
+app.use(express.static(path.join(__dirname, "public"))); //public이라는 폴더에서 css와 img를 다 받아올것이라는 의미 + public폴더 안에 css폴더와 img폴더를 넣어주면됨.
 
-app.use('/',routers);
+app.use("/", routers);
 
 module.exports = app;
